858462bc7e54f01bd9eb51f72587fd07a9580ded
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2014-12-10 17:59:53 +0900

    support no private key

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.1.0
+
+ * support no private key (fix #2)
+ 
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-eval `ssh-agent -s` && ssh-add /root/id_rsa && /gopath/bin/fleet-ui
+export ETCD_PEER=${ETCD_PEER:-"172.17.42.1"}
+
+if [ -s /root/id_rsa ]
+then
+  eval `ssh-agent -s` && ssh-add /root/id_rsa
+fi
+
+/gopath/bin/fleet-ui
