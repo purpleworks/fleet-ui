f1c4a20a33663a259b004cd05f198ed14515c695
Chungsub Kim <subicura@subicura.com>
2015-01-20 08:54:28 +0900

    fix host ip issue

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ETCD_PEER=${ETCD_PEER:-"172.17.42.1"}
+export ETCD_PEER=${ETCD_PEER:-$(/sbin/ip route|awk '/default/ { print $3 }')}
 
 if [ -f /root/id_rsa ]; then
   eval `ssh-agent -s` && ssh-add /root/id_rsa
